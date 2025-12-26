b1fb35d8656b0c7e3dcc335446217624900d91c6
Rafael Lima Loureiro <luizrafael391@gmail.com>
2025-12-26 12:34:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PostService } from './services/post.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
+  providers: [PostService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend';
+
+  constructor(private readonly _postService: PostService){}
+
+  ngOnInit(): void {
+    this._postService.getPosts().subscribe((resposta) => {
+      console.log({ resposta });
+    });
+  }
 }
